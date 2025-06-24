3a9437e302c0edc41371640e30c91f24ee30e128
HARISHMALISETTY <malisettyharish@gmail.com>
2025-06-24 11:26:06 +0530

    notes

---
+++ sub/demo.py
@@ -0,0 +1,25 @@
+# print("hello 54r")
+
+a # varible decleration
+#but this is not allow in python
+
+x=5
+
+#intialisation or assigning
+
+#python allows only intialisation/assignment but not just decleration
+
+# 3s=5 # variable name should not starts with number
+# user name="harish" #should not contain any empty spaces
+#user_name="hello" this can be allow
+# a,A these are different
+
+
+#camelcase--->userName
+#pascalcase-->UserName
+#kebabcase--->user-name this we will use in url formatting
+#snakecase--->user_name
+
+#firstname, secondname, lastname,useremail
+#name
+

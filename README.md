a6a0e087322f9f5a004afc1b4886e8150714f269
HARISHMALISETTY <malisettyharish@gmail.com>
2025-06-25 10:17:58 +0530

    optmizing and datatypes-intro

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
 # 54r
 inclass notes
+1.python installation
+2.vs code installation
+3.python execution
+4.variable and rules
+5.input()
+6.type()
+7.id()
+
+Memorypooling:-it is a concept where python trying to save memory usage by referring same object to multiple variables within limit(-5 to 256).
+
+interning:-similar to memorypooling but applicable for short strings.
+
+remembering something for future usage purpose in temporarly is caching.
+
+garbage collection:identifying unused objects and throwing them into garbage is garbage collection.
+
+we can say python is a dynamically typed language-->
+there is no any restrictions while assigning values to variables in python.
+so it can accept any type of value and changes its nature based on the value. so we can say python is a dynamically typed language.
+
+
+data types:
+-----------
+what is data type?---->nothing but data. but type of the data we use.
+data types are classified in python mainly two categories
+1.primitive
+    a.if we assign a single value to single variable then they comes under the primitive
+    b.immutable
+eg:int,float,string,complex,None,bool
+
+2.non-primitive
+    a.if we assign a multiple values to single variable then they comes unders the non-primitive
+    b.mutable
+eg:list,tuple,sets,dictionaries.
+
+list--->collection of homogenoues/heterogenous kind of elements in order.
+denotes with []
+elements in the list can be mutable.
+
+tuple--->collection of homogeneous/heterogenouos kind of elements in order
+denotes with ()
+elements in the tuple are immutable.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- variables.py
+++ variables.py
@@ -1,87 +1,108 @@
-# VARIABLES--->IS A CONTAINER TO STORE THE VALUE/INFORMATION/DATA/OBJECT  to perform operations,
-# reusage,computation,data manipulation.
+# # # VARIABLES--->IS A CONTAINER TO STORE THE VALUE/INFORMATION/DATA/OBJECT  to perform operations,
+# # # reusage,computation,data manipulation.
 
+
+# # # a=5
+
+
+# # # rules to be follow while using variables.
+
+# # # 1.variable name should not start with number
+# # # 2.variable name should not contain any white spaces
+# # # 3.variable name should not contain any spl characters except _
+# # # 4.variable names are case sensitive
+
+# # # https://www.amazon.in/gift-card-store/b/?ie=UTF8&node=3704982031&ref_=nav_cs_gc
+
+# # # type() is used to check the type of the value/data/object
+
+
+# # # num=10
+# # # num1=15
+# # # x=5.5
+# # # y=9+9j
+# # # is_qualified=True
+# # # value=None
+
+# # # print(type(num))
+# # # print(type(x))
+# # # print(type(y))
+# # # print(type(is_qualified))
+# # # print(type(value))
+# # # x=10
+# # # y=15
+# # # print(id(x))
+# # # print(id(y))
+
+# # # id()-->is used to get the id/identity of the object
+
+
+# # # x=input("enter value : ")
+# # # print(x)
+
+# # name="kiran"
+# # city="sec"
+
+# # #harish is from hyd
+
+# # # print(name,"is from",city)
+
+# # print(f"{name} is from {city}")
+
+# # # x=1
+# # # y=2
+# # # z=3
+
+# # a,b,c=1,2,3 #multiple assignement
+
+# # description="""vhjsjbkbvk.bvjkvbjkbjk
+# # vbjkvbhjkbvjkdbsdvkjdsbvjkd
+# # vbd vjknc vv v
+# # "dsvdsdvsvfsvfsg
+# # fgfgfgv
+
+# # fgsfgfgf"""
+
+# # x="fldbkfbfchwkwbf" \
+# # "hj,dchjdbcjkdbcjkdc" \
+# # "cbdjkbvdjkjdjnd" \
+# # "cbnada cbnd cmnad " \
+# # ""
+
+
+# #variable
+# #intialisation and assigning
+# #rules to be follow for variable names
+# #naming conventions
+# #type()-->to check the type of the value/data/object
+# #id()--->to check the id/identity of the object
+# #input()--->to take the inputs from the user
+# #formatting string
+# #multiline strings
+# #comments
+
+
+# a=10
+
+# b=10
+
+# c=10
+
+# # 10 is referring to 3 variables
 
 # a=5
+# b=6
+# c=8
+
+# #10 is referring to 0 variables
 
 
-# rules to be follow while using variables.
-
-# 1.variable name should not start with number
-# 2.variable name should not contain any white spaces
-# 3.variable name should not contain any spl characters except _
-# 4.variable names are case sensitive
-
-# https://www.amazon.in/gift-card-store/b/?ie=UTF8&node=3704982031&ref_=nav_cs_gc
-
-# type() is used to check the type of the value/data/object
+# ip=input("enter num")
+# # print(type(ip))
+# # cnvrt=int(ip)
+# cnvrt=float(ip)
+# print(cnvrt)
+# print(type(cnvrt))
 
 
-# num=10
-# num1=15
-# x=5.5
-# y=9+9j
-# is_qualified=True
-# value=None
 
-# print(type(num))
-# print(type(x))
-# print(type(y))
-# print(type(is_qualified))
-# print(type(value))
-# x=10
-# y=15
-# print(id(x))
-# print(id(y))
-
-# id()-->is used to get the id/identity of the object
-
-
-# x=input("enter value : ")
-# print(x)
-
-name="kiran"
-city="sec"
-
-#harish is from hyd
-
-# print(name,"is from",city)
-
-print(f"{name} is from {city}")
-
-# x=1
-# y=2
-# z=3
-
-a,b,c=1,2,3 #multiple assignement
-
-description="""vhjsjbkbvk.bvjkvbjkbjk
-vbjkvbhjkbvjkdbsdvkjdsbvjkd
-vbd vjknc vv v
-"dsvdsdvsvfsvfsg
-fgfgfgv
-
-fgsfgfgf"""
-
-x="fldbkfbfchwkwbf" \
-"hj,dchjdbcjkdbcjkdc" \
-"cbdjkbvdjkjdjnd" \
-"cbnada cbnd cmnad " \
-""
-
-
-#variable
-#intialisation and assigning
-#rules to be follow for variable names
-#naming conventions
-#type()-->to check the type of the value/data/object
-#id()--->to check the id/identity of the object
-#input()--->to take the inputs from the user
-#formatting string
-#multiline strings
-#comments
-
-
-#using input take name,age,qualification,city
-
-#format a single line string with those values and print

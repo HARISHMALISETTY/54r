71a89431f59d902bbda3049db088ee78572df176
HARISHMALISETTY <malisettyharish@gmail.com>
2025-07-03 14:06:59 +0530

    conditional statements

---
+++ cs.py
@@ -0,0 +1,85 @@
+# statements which executes based on a specific condition,
+# then it can be called as conditional statements
+
+#these can be use for decision making purpose
+
+#we have multiple types in conditional statements:
+
+# 1.simple-if 
+    # statements will executes whenever condition is true only
+# 2.if-else
+    # whenever we want to execute one more statement as a alternate if true statement 
+    # doesn't executes
+    # for running false statement, we can use else . 
+# 3.nested-if 
+# 4.elif 
+# 5.terinary operator
+
+# print("hello world")
+a=10
+b=16
+
+# if(a+b==25):
+#     print("welcome")
+# if(a+b<=30):
+#     print("hii")
+# if(a+b>=20):
+#     print("hellooo")
+
+# "maheshbabu","pspk","AA","NTR","Prabhas"
+# Mb_Contact=True 
+# Pspk_Contact=True
+# AA_Contact=True
+# Ntr_Contact=False
+# Prabhas_Contact=False
+# Mb_will_Answer=True
+# Pspk_will_Answer=False
+# AA_will_Answer=True
+# Ntr_will_Answer=True
+# Prabhas_will_answer=True
+
+# if(Mb_Contact and Mb_will_Answer):
+#     print("i can call to Maheshbabu")
+# if(Pspk_Contact and Pspk_will_Answer):
+#     print("i can call to pspsk")
+# if(AA_Contact and AA_will_Answer):
+#     print("i can call to AA")
+# if(Ntr_Contact and Ntr_will_Answer):
+#     print("i can call to Ntr")
+# if(Prabhas_Contact and Prabhas_will_answer):
+#     print("i can call to prabhas")  
+
+# Mb_contact=False
+# if(Mb_contact):
+#     print("can call to MB")
+# else:
+#     print("call to his PA")
+
+# num=52
+# if (num%2==0) :
+#     print(f"{num} is even")
+# else:
+#     print(f"{num} is odd")
+
+# num=125289
+# if(num & 1 ==0):
+#     print(f"{num} is even")
+# else:
+#     print(f"{num} is odd") 
+
+
+#in operator
+# num=125012589
+# cnvrt=str(num)
+# # print(cnvrt[-1])
+
+# seq=[0,2,4,6,8]
+# if(int(cnvrt[-1]) in seq):
+#     print(f"{num} is even")
+# else:
+#     print(f"{num} is odd")
+
+# seq="02468"
+
+# num=1254589
+# ld=num%10
---
+++ operators.py
@@ -0,0 +1,21 @@
+#identity is and is not
+#membership
+
+
+str="welcome"
+
+# print("c" in str)
+
+seq=[1,2,3,4,5,6]
+# print(4 in seq)
+
+tup=("a","b","c","ref")
+# print("x" in tup)
+
+# dict={"name":"harish","city":"hyd","country":"India"}
+
+# print("country" in dict)
+
+set1={1,2,3,4,5}
+
+print('3' not in set1)

3031f629bb0774df1266be79d5eff45a3902399f
HARISHMALISETTY <malisettyharish@gmail.com>
2025-07-30 08:24:02 +0530

    inheritance

--- inheritance.py
+++ inheritance.py
@@ -12,21 +12,78 @@
 # obj1=Child()
 # obj1.Pmethod2()
 
-class Chiranjeevi:
-    def C_assets(self):
-        self.c_worth=100
-        print(f"chiranjeevi assets-{self.c_worth} cr")
+# class Chiranjeevi:
+#     def C_assets(self):
+#         self.c_worth=100
+#         print(f"chiranjeevi assets-{self.c_worth} cr")
 
-class RamCharan(Chiranjeevi):
-    def R_assets(self):
-        self.r_worth=50
-        print(f"ram charan- worth {self.r_worth} cr")
-    def Total_assets(self):
-        super().C_assets()
-        total_worth=self.r_worth+self.c_worth
-        print(f"{total_worth}cr")
+# class RamCharan(Chiranjeevi):
+#     def R_assets(self):
+#         self.r_worth=50
+#         print(f"ram charan- worth {self.r_worth} cr")
+#     def Total_assets(self):
+#         super().C_assets()
+#         total_worth=self.r_worth+self.c_worth
+#         print(f"{total_worth}cr")
 
-rc_obj=RamCharan()
-rc_obj.R_assets()
-rc_obj.C_assets()
-rc_obj.Total_assets()
+# rc_obj=RamCharan()
+# rc_obj.R_assets()
+# rc_obj.C_assets()
+# rc_obj.Total_assets()
+
+# class ParentActor:
+#     def __init__(self,Pname,Pworth):
+#         self.Pname=Pname
+#         self.Pworth=Pworth
+#         print(f"{self.Pname} is the parent")
+#     def Passets(self):
+#         print(f"{self.Pname} assets are {self.Pworth} cr")
+# class ChildActor(ParentActor):    
+#     def __init__(self,Pname,Cname,Pworth,Cworth):
+#         super().__init__(Pname,Pworth)
+#         self.Cname=Cname
+#         self.Cworth=Cworth
+#         print(f"{self.Cname}is came by the referrence of{self.Pname}")
+#     def Cassets(self):
+#         print(f"{self.Cname}is having worth of {self.Cworth}cr")
+#     def TotalAssets(self):
+#         print(f"total assets of {self.Cname} is {self.Pworth+self.Cworth}")
+# class grandChildActor(ChildActor):
+#     def __init__(self,Gname,Pname,Cname,Pworth,Cworth,Gworth):
+#         self.Gname=Gname
+#         self.Gworth=Gworth
+#         super().__init__(Pname,Cname,Pworth,Cworth)
+#     def Gassets(self):
+#         print(f"{self.Gname} is having assets of {self.Gworth}")
+#     def GTotalAssets(self):
+#         print(f"{self.Gname}is having total assets of {self.Pworth+self.Cworth+self.Gworth}")
+
+# gowtham=grandChildActor(Gname='gowtham',Cname='Mahesh',Pname='krishna',Cworth=50,Gworth=25,Pworth=100)
+# gowtham.GTotalAssets()
+# ramcharan=ChildActor("chiranjeevi","ramcharan",100)
+# ramcharan.Cassets(75)
+# ramcharan.TotalAssets()
+# ramcharan.Passets()
+
+class MegaStar:
+    def __init__(self,M_Worth):
+        self.M_Worth=M_Worth
+        print(f"megastar having worth of{self.M_Worth}cr")
+class PowerStar:
+    def __init__(self,P_Worth):
+        self.P_Worth=P_Worth
+        print(f"powerstar having worth of{self.P_Worth}cr")
+class MegaPowerStar():
+    def __init__(self,Mp_Worth,M_Worth,P_Worth):
+       self.Mp_Worth=Mp_Worth      
+       MegaStar.__init__(self,M_Worth)
+       PowerStar.__init__(self,P_Worth)
+    def getMegaPowerStarAssets(self):
+        print(f"{self.Mp_Worth}")
+
+rc=MegaPowerStar(Mp_Worth=150,M_Worth=100,P_Worth=100)
+# jc=MegaPowerStar(Mp_Worth=150,M_Worth=100,P_Worth=100)
+rc.getMegaPowerStarAssets()
+
+
+
